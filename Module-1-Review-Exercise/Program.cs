2941cdf15da1df6c349241987fd42702f11defb3
Bryan Durushia <b.durushia@gmail.com>
2024-02-16 13:52:12 -0600

    Added comments

--- Module-1-Review-Exercise/Program.cs
+++ Module-1-Review-Exercise/Program.cs
@@ -5,15 +5,20 @@
 
         static double GetCircleArea(double radius)
         {
+            // This method takes in the user given radius and uses Math.PI to calculate the area of the circle
             double circleArea = Math.PI * radius * radius;
+            // return the calculated area of the circle
             return circleArea;
         }
         static void Main(string[] args)
         {
+            // Ask user for a radius of a circle, listen for user data and convert it to a double
             Console.WriteLine($"Please enter your circle radius number:");
             double radius = Convert.ToDouble(Console.ReadLine());
+            // Calls circle area method and writes calculated area to console
             Console.WriteLine($"The area of the circle that has a radius of {radius} is {GetCircleArea(radius)}");
 
+            // Ensures user can exit the CLI application after everything is done
             Console.WriteLine("Press any key to exit the application.");
             Console.ReadKey();
         }
