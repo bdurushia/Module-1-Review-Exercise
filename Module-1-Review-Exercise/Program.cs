0079de58bd1211fab6c25a3fdcdb242faf594c5f
Bryan Durushia <b.durushia@gmail.com>
2024-02-16 13:41:07 -0600

    Built a method to calculate the area of a circle with user provided radius.

--- Module-1-Review-Exercise/Program.cs
+++ Module-1-Review-Exercise/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+
+        static double GetCircleArea(double radius)
+        {
+            double circleArea = Math.PI * radius * radius;
+            return circleArea;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Please enter your circle radius number:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The area of the circle that has a radius of {radius} is {GetCircleArea(radius)}");
+
+            Console.WriteLine("Press any key to exit the application.");
+            Console.ReadKey();
         }
     }
 }
