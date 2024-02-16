0ceba844b2b5b48db95784538ab8013ec66ab10f
Bryan Durushia <b.durushia@gmail.com>
2024-02-16 14:19:49 -0600

    Created a method to calculate area of circle

--- Module-1-Review-Exercise/Program.cs
+++ Module-1-Review-Exercise/Program.cs
@@ -10,16 +10,35 @@
             // return the calculated area of the circle
             return circleArea;
         }
+
+        static double GetTriangleArea(double baseLength, double height)
+        {
+            // A = ( hb * b ) / 2
+            double triangleArea = (height * baseLength) / 2;
+            return triangleArea;
+
+        }
         static void Main(string[] args)
         {
+            // AREA OF CIRCLE
             // Ask user for a radius of a circle, listen for user data and convert it to a double
-            Console.WriteLine($"Please enter your circle radius number:");
+            Console.WriteLine("Please enter your circle radius number:");
             double radius = Convert.ToDouble(Console.ReadLine());
             // Calls circle area method and writes calculated area to console
-            Console.WriteLine($"The area of the circle that has a radius of {radius} is {GetCircleArea(radius)}");
+            Console.WriteLine($"The area of your circle is {GetCircleArea(radius)}.\n");
+
+            // AREA OF TRIANGLE
+            // Ask for height of triangle, convert to double and store it
+            Console.WriteLine("Please enter the height of your triangle:");
+            double height = Convert.ToDouble(Console.ReadLine());
+            // Ask for base length of triangle, convert to double and store it
+            Console.WriteLine("Please enter the base length of your triangle:");
+            double baseLength = Convert.ToDouble(Console.ReadLine());
+            // Call triangle method and print out the calculated area of the triangle
+            Console.WriteLine($"The area of your triangle is {GetTriangleArea(height, baseLength)}.");
 
             // Ensures user can exit the CLI application after everything is done
-            Console.WriteLine("Press any key to exit the application.");
+            Console.WriteLine("\nPress any key to exit the application.");
             Console.ReadKey();
         }
     }
--- Module-1-Review-Exercise/obj/Debug/net8.0/Module-1-Review-Exercise.AssemblyInfo.cs
+++ Module-1-Review-Exercise/obj/Debug/net8.0/Module-1-Review-Exercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module-1-Review-Exercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f502093e77c3eaa9b2fc1d0e53501729c588a097")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2941cdf15da1df6c349241987fd42702f11defb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module-1-Review-Exercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module-1-Review-Exercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
